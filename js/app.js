e4cff08e666c90d7cde2b86a47139ab7deafcfc3
onuproy <anupme01@gmail.com>
2021-09-14 16:49:20 +0600

    image show design fix rating

--- js/app.js
+++ js/app.js
@@ -12,11 +12,13 @@ const showProducts = (products) => {
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
+      <div class="single-product-image">
+          <img class="product-image" src=${product.image}></img>
       </div>
       <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
+      <p><strong>Category:</strong> ${product.category}</p>
+      <p><strong>Average Ratings:</strong> ${product.rating.rate}</p>
+      <p><strong>Total Ratings:</strong> ${product.rating.count}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
